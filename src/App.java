c245d16ad86c8953474ad6ea5696f797af1514ca
petertechdev <peterleite.dev@mail.com>
2022-07-24 09:16:51 -0300

    first goals in the project

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Peter");
+
+        // pegar dados do IMDB (conexão HTTP)
+        // buscar os top 250 filmes
+        // extrair dados interessantes (titulo, poster, classificação)
+        //exibir e manipular os dados
+    }
+}
